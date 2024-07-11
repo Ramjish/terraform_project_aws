c264fd8ad2e7480acba8a2ced25078eefbb65683
Ramji <ramji@gmail.com>
2024-07-11 11:39:55 +0530

    my first repo creation in github with help of terraform code

---
+++ github.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 6.0"
+    }
+  }
+}
+
+# Configure the GitHub Provider
+provider "github" {
+
+    token = "Personal access tokens"
+}
+
+resource "github_repository" "terraform_repo" {
+  name        = "Terraform_repo"
+  description = "My first repo create with terraform"
+
+  visibility = "public"
+
+  
+  }
+
+
+  
